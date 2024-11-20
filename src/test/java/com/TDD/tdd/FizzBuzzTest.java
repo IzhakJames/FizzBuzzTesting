5546495c57c9af5fa3f6928fcec2fb18204d2275
Mohd Izhak <mohdizhak@Mohds-MacBook-Pro.local>
2024-11-20 14:19:26 +0800

    Removed 1 test

--- src/test/java/com/TDD/tdd/FizzBuzzTest.java
+++ src/test/java/com/TDD/tdd/FizzBuzzTest.java
@@ -70,12 +70,12 @@ class FizzBuzzTest {
         assertEquals(expected, FizzBuzz.compute(value));
     }
 
-    @DisplayName("Testing with Large data file")
-    @ParameterizedTest(name="value={0}, expected={1}")
-    @CsvFileSource(resources="/large-test-data.csv")
-    @Order(7)
-    @Tag("LargeDataFile") // Tagging with LargeDataFile
-    void testLargeDataFile(int value, String expected) {
-        assertEquals(expected, FizzBuzz.compute(value));
-    }
+//    @DisplayName("Testing with Large data file")
+//    @ParameterizedTest(name="value={0}, expected={1}")
+//    @CsvFileSource(resources="/large-test-data.csv")
+//    @Order(7)
+//    @Tag("LargeDataFile") // Tagging with LargeDataFile
+//    void testLargeDataFile(int value, String expected) {
+//        assertEquals(expected, FizzBuzz.compute(value));
+//    }
 }
